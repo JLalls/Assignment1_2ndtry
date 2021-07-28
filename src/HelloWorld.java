56d27d01e9a29bb1bf8d6c975cd5b3814f61e707
John Lally <john.jack.lally@gmail.com>
2021-07-27 22:02:03 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,32 @@
+import java.security.DigestException;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    System.out.println("Hello World");
+    }
+
+    public static class Animal {
+        private String name;
+        private int legs;
+        private String speak;
+
+
+        public Animal(String name) {
+            this.name = "Dog";
+            this.legs = 4;
+            this.speak = "Woof!";
+
+            public Animal(String name) {
+                this.name = "Snake";
+                this.legs = 0;
+                this.speak = "Hisssss";
+
+                public Animal(String name) {
+                    this.name = "Human";
+                    this.legs = 2;
+                    this.speak = "Hello, there!"
+                }
+            }
+        }
+    }
+}
